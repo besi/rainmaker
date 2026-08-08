d8ddf86cd83eb0961a63d9733780a6ae306ebeda
Beat Besmer <beat.besmer@gmail.com>
2026-08-08 10:14:37 +0200

    New pin mapping for the pcb and mute functionality

--- sw/main.py
+++ sw/main.py
@@ -6,22 +6,26 @@ from machine import Pin
 from machine import SoftSPI, Pin, SDCard
 import os
 
-sd = SDCard(sck=1, miso=0, mosi=2, cs=3)
-os.mount(sd, "/sd")  # mount
+sd = SDCard(sck=0, miso=1, mosi=4, cs=5)
 
+os.mount(sd, "/sd")
 
 from wavplayer import WavPlayer
 
-BUFFER_LENGTH_IN_BYTES = 2000
+
 I2S_ID = 0
+BUFFER_LENGTH_IN_BYTES = 2000
+
 
 wp = WavPlayer(
     id=I2S_ID,
     sck_pin=Pin(10),
     ws_pin=Pin(6),
     sd_pin=Pin(8),
-    ibuf=2000,
+    ibuf=BUFFER_LENGTH_IN_BYTES,
 )
+mute = Pin(2, Pin.OUT)
+mute.off()
 
 wp.play("rain.wav", loop=True)
 while wp.isplaying() == True:
